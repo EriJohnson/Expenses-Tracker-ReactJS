03d5abb0f014acf19921dd12e2efe394693db464
Eri Johnson <erijohnsons.fernandes@gmail.com>
2020-11-21 18:11:49 -0300

    feat: create Header component and add styles to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Header } from './components/Header'
+
 import './App.css'
 
 function App() {
-  return <div className='App'>Hello World</div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App
